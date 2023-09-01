e6fe046ce603172e20f8394bb36d7eebe1387fbb
UTGLANCEELOT <143734598+UTGLANCEELOT@users.noreply.github.com>
2023-09-01 08:58:38 +0800

    Portfolio

---
+++ script.js
@@ -0,0 +1,7 @@
+// JavaScript code for interactivity
+document.addEventListener('DOMContentLoaded', function() {
+    const header = document.querySelector('header');
+    header.addEventListener('click', function() {
+        alert('"Isispin mo ang maganda mong kinabukasan, hindi yung puro yung walang ka kwenta kwentang bagay, hindi yung puro katarantaduhan at kawalanghiyaan. Bagkos magsumikap ka, tyagain mo ang pag-aaral mo at yaang mga pangarap mo, lahat yan ay makukuha mo kapag ikaw ay matiyagang nag aral."');
+    });
+});
